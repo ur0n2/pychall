911363b07f3e70240c06c953c485118b8d59a883
ur0n2 <ur0n2@sju.ac.kr>
2015-06-13 08:38:43 +0000

    solved

---
+++ pychall1.py
@@ -0,0 +1,6 @@
+t='g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+
+import string
+lower=string.lowercase #a~z
+text=string.maketrans(lower,lower[2:]+lower[:2]) # lower translate to c~z+a+b
+print t.translate(text)
